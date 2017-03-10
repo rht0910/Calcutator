b43fc8f7a3c4515d717406b5db8d6c67f370bcc1
rht0910 <rht0910@outlook.jp>
2017-03-10 17:37:30 +0900

    AssemblyInfo.cs fixed. and Release file created(/Calcurator/Setup.zip)!

--- Calcutator/Properties/AssemblyInfo.cs
+++ Calcutator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Calcutator")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © @rht0910  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
